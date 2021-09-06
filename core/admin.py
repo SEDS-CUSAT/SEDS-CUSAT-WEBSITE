510da986a1d4578faf50725deade19d142c6cad2
ashfaqnaha <ashfaqnaha2000@gmail.com>
2021-09-06 18:24:40 +0530

    changed models.py file

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import About, Project, Blog, Team, Alumni, Gallery
+from .models import About, Project, Blog, Team, Alumni, Gallery,Event,SocialMedia
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(SocialMedia)
 admin.site.register(About)
 admin.site.register(Project)
 admin.site.register(Blog)
--- core/models.py
+++ core/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Sharun's model
 # About model
 class About(models.Model):
     image = models.ImageField(default="image", upload_to="about/")
-
-
+    description = models.CharField(max_length=3000, default="About Description")
+def __str__(self):
+        return self.title
 # Event model
 
 
@@ -15,6 +17,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100, default="Project Title")
     description = models.CharField(max_length=300, default="Project Description")
     image = models.ImageField(default="image", upload_to="project/")
+    link = models.CharField(max_length=100,default="#")
 
     def __str__(self):
         return self.title
@@ -31,22 +34,54 @@ class Blog(models.Model):
     def __str__(self):
         return self.title
 
-
 # Team model
 class Team(models.Model):
     name = models.CharField(max_length=100, default="name")
     designation = models.CharField(max_length=100, default="designation")
     image = models.ImageField(default="image", upload_to="team/")
 
+    def __str__(self):
+        return self.title
+
 
 # Alumni model
 class Alumni(models.Model):
     name = models.CharField(max_length=100, default="name")
     designation = models.CharField(max_length=100, default="designation")
+    description = models.CharField(max_length=1200, default="description")
     image = models.ImageField(default="image", upload_to="alumni/")
     social = models.URLField(max_length=250)
 
 
+    def __str__(self):
+        return self.title
+
 # Gallery model
 class Gallery(models.Model):
     image = models.ImageField(default="image", upload_to="gallery/")
+
+    def __str__(self):
+        return self.title
+#event model
+class Event(models.Model):
+
+    title = models.CharField(max_length=100, default="Event title")
+    location = models.CharField(max_length=100, default="Event location")
+    description = models.TextField(max_length=1200, default="description")
+    image = models.ImageField(default="image",upload_to="events/")
+    event_day = models.DateField(default=timezone.now())
+    event_time = models.TimeField(default=timezone.now())
+    registration_link = models.CharField(max_length=100, default="#")
+def __str__(self):
+    return self.title
+
+class SocialMedia(models.Model):
+
+    insta_link = models.CharField(max_length=100, default="#")
+    fb_link = models.CharField(max_length=100, default="#")
+    twitter_link = models.CharField(max_length=100, default="#")
+    linkedin_link = models.CharField(max_length=100, default="#")
+    medium_link = models.CharField(max_length=100, default="#")
+def __str__(self):
+        return self.title
+
